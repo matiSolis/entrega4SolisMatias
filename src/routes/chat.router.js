c3fff1ce73b3a7ffea9e7c46de534f72e45022e2
matiSolis <solis.matias.n@gmail.com>
2023-05-08 00:38:38 -0300

    cart router falta agregar producto al cart bis

--- src/routes/chat.router.js
+++ src/routes/chat.router.js
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import messagesModel from "../Dao/models/messages.model.js";
+import ManagerAcces from "../Dao/managers/managerAcces.js";
 
 const router = Router();
+const managerAcces = new ManagerAcces();
 
 router.get('/', async (req, res) => {
     try {
+        await managerAcces.createRecord('GET MESSAGES');
         const messages = await messagesModel.find();
-        res.status(200).render('chat', {messages});
+        res.status(200).render({messages});
     }catch (err) {
         res.status(400).send({
             status: "Error",
